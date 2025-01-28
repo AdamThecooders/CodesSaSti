9637bb6eb1d2e03fda68bd7a17f84a14017629fa
AdamThecooders <wealthadam23@gmail.com>
2025-01-28 08:50:13 +0800

    Add files via upload

---
+++ SQLQuery Book database.sql
@@ -0,0 +1,6 @@
+Insert Into Books Values (39,'The Great Gatsby','F.Scott Fitzgerald','Fiction',10.99,1925),
+                         (23,'to kill a Mockingbird','Harper Lee','Fiction',8.99,1960),
+						 (12,'1984','George Orwell','Dystopian',  9.49,1949),
+						 (2,'The Catcher in the Rye','J.D. Salinger','Fiction',7.99, 1951),
+						 (3,'The Hobbit','J.R.R. Tolkien','Fantasy',12.99,1937);
+
